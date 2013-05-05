17648306c8e6152a0c8bce3b24b70339293fa889
Mats Vederhus <afr0_at_afr0games.com>
2013-05-05 09:37:27 +0200

    Finished licensing client.

--- TSOClient/TSOClient/Code/UI/Controls/UIGridViewer.cs
+++ TSOClient/TSOClient/Code/UI/Controls/UIGridViewer.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*The contents of this file are subject to the Mozilla Public License Version 1.1
+(the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is the TSOClient.
+
+The Initial Developer of the Original Code is
+ddfczm. All Rights Reserved.
+
+Contributor(s): ______________________________________.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,19 +27,7 @@ namespace TSOClient.Code.UI.Controls
 {
     public class UIGridViewer : UIContainer
     {
-
-        /**
-         * private int myThumbSizeX, myThumbSizeY, myThumbMarginX, myThumbMarginY, myThumbImageSizeX, myThumbImageSizeY, myThumbImageOffsetX, myThumbImageOffsetY, myRows, myColumns;
-         */
-
-
-
         public event ChangeDelegate OnChange;
-
-
-        /**
-         * Settings
-         */
 
         /// <summary>
         /// Class to use as the item renderer for each cell in the grid
--- TSOClient/TSOClient/Lot/Wall.cs
+++ TSOClient/TSOClient/Lot/Wall.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*The contents of this file are subject to the Mozilla Public License Version 1.1
+(the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is the TSOClient.
+
+The Initial Developer of the Original Code is
+Afr0. All Rights Reserved.
+
+Contributor(s): ______________________________________.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
