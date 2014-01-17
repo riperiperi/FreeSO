bbec2793e75fc06bacdd66c18436ab10b0b3a669
Mats Vederhus <afr0_at_afr0games.com>
2014-01-17 18:11:48 +0100

    Added new Manifestation.
    Keep It Simple, Stupid!

---
+++ Other/tools/Manifestation/Manifestation/ManifestFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Manifestation
+{
+    /// <summary>
+    /// A manifest file is a file that has a version and references a bunch of patch files.
+    /// </summary>
+    class ManifestFile
+    {
+        public ManifestFile(string Path, string Version, List<PatchFile> PatchFiles)
+        {
+            bool HasURLs = false;
+            BinaryWriter Writer = new BinaryWriter(File.Create(Path));
+            Writer.Write((string)Version);
+
+            if (PatchFiles[0].URL != "")
+                HasURLs = true;
+
+            foreach (PatchFile PFile in PatchFiles)
+            {
+                if (!HasURLs)
+                    Writer.Write((string)PFile.Address + "," + PFile.FileHash);
+                else
+                    Writer.Write((string)PFile.Address + "," + PFile.FileHash + PFile.URL);
+            }
+
+            Writer.Flush();
+            Writer.Close();
+        }
+    }
+}
