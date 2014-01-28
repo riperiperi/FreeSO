e23a0912e3ba9a37724fb63759fe677156dd3eb9
Mats Vederhus <afr0_at_afr0games.com>
2014-01-28 21:10:29 +0100

    Fixed more bugs.
    Fixed string encoding bugs when reading a PacketStream in GonzoNet.
    Made PDBootstrap enforce admin UAC when running PDPatcher.
    Fixed a crashing bug in the client's InputManager.

--- PDPatcher/PDBootstrap/PDBootstrap/Program.cs
+++ PDPatcher/PDBootstrap/PDBootstrap/Program.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Diagnostics;
+using System.Security.Principal;
 
 namespace PDBootstrap
 {
     class Program
     {
+        /// <summary>
+        /// The function checks whether the current process is run as administrator.
+        /// In other words, it dictates whether the primary access token of the 
+        /// process belongs to user account that is a member of the local 
+        /// Administrators group and it is elevated.
+        /// </summary>
+        /// <returns>
+        /// Returns true if the primary access token of the process belongs to user 
+        /// account that is a member of the local Administrators group and it is 
+        /// elevated. Returns false if the token does not.
+        /// </returns>
+        internal static bool IsRunAsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(id);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
         static void Main(string[] args)
         {
             string WorkingDir = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
@@ -29,6 +48,33 @@ namespace PDBootstrap
             {
                 File.Delete(TSOPatchDir + "\\PDPatcher.exe.config");
                 File.Move(WorkingDir + "\\PDPatcher.exe.config", TSOPatchDir + "\\PDPatcher.exe.config");
+            }
+
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                if (!IsRunAsAdmin())
+                {
+                    // Launch itself as administrator
+                    ProcessStartInfo proc = new ProcessStartInfo();
+                    proc.UseShellExecute = true;
+                    proc.WorkingDirectory = TSOPatchDir;
+                    proc.FileName = TSOPatchDir + "\\PDPatcher.exe";
+                    proc.Verb = "runas";
+
+                    try
+                    {
+                        Process.Start(proc);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("You need to run PDPatcher as admin!");
+                        Console.ReadKey();
+
+                        return;
+                    }
+
+                    Environment.Exit(0);  //Quit
+                }
             }
 
             //Bootstrap...
