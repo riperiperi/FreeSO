5baefbdec816307de5a9f3adc4a511d878448a71
Mats Vederhus <afr088@hotmail.com>
2014-03-17 09:23:57 +0100

    Update UISim.cs
    Added a GUID to constructor - creating a Sim without one is dangerous!

--- TSOClient/TSOClient/Code/UI/Controls/UISim.cs
+++ TSOClient/TSOClient/Code/UI/Controls/UISim.cs
@@ -203,7 +203,7 @@ namespace TSOClient.Code.UI.Controls
             set { m_CreatedThisSession = value; }
         }
 
-        public UISim()
+        public UISim(string GUID)
         {
             Camera = new BasicCamera(GameFacade.GraphicsDevice, new Vector3(0.0f, 7.0f, -17.0f), Vector3.Zero, Vector3.Up);
             Scene = new _3DScene(GameFacade.Game.GraphicsDevice, Camera);
