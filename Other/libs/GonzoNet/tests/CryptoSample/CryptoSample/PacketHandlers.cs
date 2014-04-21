f61e377cc2474cffcd46151a89fc01b40178c775
Mats Vederhus <afr0_at_afr0games.com>
2014-04-21 21:05:05 +0200

    Implemented secure random byte generation
    GUID isn't cryptographically secure, apparently.

--- Other/libs/GonzoNet/tests/CryptoSample/CryptoSample/PacketHandlers.cs
+++ Other/libs/GonzoNet/tests/CryptoSample/CryptoSample/PacketHandlers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Security.Cryptography;
 using System.IO;
+using System.Linq;
 using GonzoNet;
 using GonzoNet.Encryption;
 
@@ -15,9 +16,9 @@ namespace CryptoSample
         public static ECDiffieHellmanCng ClientKey = new ECDiffieHellmanCng(m_MasterKey), 
             ServerKey = new ECDiffieHellmanCng(m_MasterKey);
 
-        //private static AESEncryptor m_Encryptor;
-        private static Guid ChallengeResponse = Guid.NewGuid();
-        public static Guid ClientNOnce = Guid.NewGuid();
+        private static RNGCryptoServiceProvider m_Random = new RNGCryptoServiceProvider();
+        private static byte[] ChallengeResponse;
+        public static byte[] ClientNOnce;
 
         /// <summary>
         /// A client requested login.
@@ -28,7 +29,7 @@ namespace CryptoSample
         {
             Console.WriteLine("Server receives data - test 1");
 
-            Guid Nonce = new Guid(Packet.ReadPascalString());
+            byte[] Nonce = Packet.ReadBytes(16);
 
             //Username would normally be used to lookup client's public key in DB (only time such a use is valid).
             string Username = Packet.ReadPascalString();
@@ -40,17 +41,20 @@ namespace CryptoSample
             byte[] ClientKey = Client.ClientEncryptor.GetDecryptionArgsContainer().AESDecryptArgs.Key;
             byte[] ClientIV = Client.ClientEncryptor.GetDecryptionArgsContainer().AESDecryptArgs.IV;
 
+            ChallengeResponse = new byte[16];
+            m_Random.GetNonZeroBytes(ChallengeResponse);
+
             MemoryStream StreamToEncrypt = new MemoryStream();
             BinaryWriter Writer = new BinaryWriter(StreamToEncrypt);
-            Writer.Write((byte)ChallengeResponse.ToByteArray().Length);
-            Writer.Write(ChallengeResponse.ToByteArray(), 0, ChallengeResponse.ToByteArray().Length);
+            Writer.Write((byte)ChallengeResponse.Length);
+            Writer.Write(ChallengeResponse, 0, ChallengeResponse.Length);
             Writer.Write((byte)ClientKey.Length);
             Writer.Write(ClientKey, 0, ClientKey.Length);
             Writer.Write((byte)ClientIV.Length);
             Writer.Write(ClientIV, 0, ClientIV.Length);
             Writer.Flush();
 
-            byte[] EncryptedData = StaticStaticDiffieHellman.Encrypt(ServerKey, ClientPub, Nonce.ToByteArray(), 
+            byte[] EncryptedData = StaticStaticDiffieHellman.Encrypt(ServerKey, ClientPub, Nonce, 
                 StreamToEncrypt.ToArray());
 
             EncryptedPacket.WriteUInt16((ushort)(PacketHeaders.UNENCRYPTED + 1 + EncryptedData.Length));
@@ -78,18 +82,18 @@ namespace CryptoSample
             ECDiffieHellmanPublicKey ServerPub = StaticStaticDiffieHellman.ImportKey("ServerPublic.dat");
 
             MemoryStream DecryptedStream = new MemoryStream(StaticStaticDiffieHellman.Decrypt(ClientKey, ServerPub, 
-                ClientNOnce.ToByteArray(), PacketBuf));
+                ClientNOnce, PacketBuf));
             BinaryReader Reader = new BinaryReader(DecryptedStream);
 
-            Guid ChallengeResponse = new Guid(Reader.ReadBytes(Reader.ReadByte()));
+            byte[] ChallengeResponse = Reader.ReadBytes(Reader.ReadByte());
 
             //Yay, we have key and IV, we can now start encryption with AES!
             Client.ClientEncryptor = new AESEncryptor(Reader.ReadBytes(Reader.ReadByte()), Reader.ReadBytes(Reader.ReadByte()), "");
 
             MemoryStream StreamToEncrypt = new MemoryStream();
             BinaryWriter Writer = new BinaryWriter(StreamToEncrypt);
-            Writer.Write((byte)ChallengeResponse.ToByteArray().Length);
-            Writer.Write(ChallengeResponse.ToByteArray(), 0, ChallengeResponse.ToByteArray().Length);
+            Writer.Write((byte)ChallengeResponse.Length);
+            Writer.Write(ChallengeResponse, 0, ChallengeResponse.Length);
 
             //Encrypt data using key and IV from server, hoping that it'll be decrypted correctly at the other end...
             Client.SendEncrypted(0x03, StreamToEncrypt.ToArray());
@@ -101,9 +105,9 @@ namespace CryptoSample
         {
             Console.WriteLine("Server receives challenge response - test 3");
 
-            Guid CResponse = new Guid(Packet.ReadBytes(Packet.ReadByte()));
+            byte[] CResponse = Packet.ReadBytes(Packet.ReadByte());
 
-            if (CResponse.CompareTo(ChallengeResponse) == 0)
+            if (CResponse.SequenceEqual(ChallengeResponse))
                 Console.WriteLine("Received correct challenge response, client was authenticated!");
 
             Console.WriteLine("Test 3: passed!");
--- Other/libs/GonzoNet/tests/CryptoSample/CryptoSample/PacketSenders.cs
+++ Other/libs/GonzoNet/tests/CryptoSample/CryptoSample/PacketSenders.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Security.Cryptography;
 using GonzoNet;
 
 namespace CryptoSample
@@ -10,12 +11,17 @@ namespace CryptoSample
         //First packet sent from client to server.
         public static void SendInitialConnectPacket(NetworkClient Client, string Username)
         {
+            RNGCryptoServiceProvider Random = new RNGCryptoServiceProvider();
+
             PacketStream InitialPacket = new PacketStream(0x01, 0);
             InitialPacket.WriteHeader();
 
+            PacketHandlers.ClientNOnce = new byte[16];
+            Random.GetNonZeroBytes(PacketHandlers.ClientNOnce);
+
             InitialPacket.WriteUInt16((ushort)((byte)PacketHeaders.UNENCRYPTED + 
                 (PacketHandlers.ClientNOnce.ToString().Length + 1) + (Username.Length + 1)));
-            InitialPacket.WritePascalString(PacketHandlers.ClientNOnce.ToString());
+            InitialPacket.WriteBytes(PacketHandlers.ClientNOnce);
             InitialPacket.WritePascalString(Username);
 
             Client.Send(InitialPacket.ToArray());
