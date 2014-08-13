881430274657d2fe454b010f0b1dd51c4e8c81a5
Mats Vederhus <afr0_at_afr0games.com>
2014-08-13 17:12:49 +0200

    Started adding chat feature.

--- TSO CityServer/TSO CityServer/Network/NetworkFacade.cs
+++ TSO CityServer/TSO CityServer/Network/NetworkFacade.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*The contents of this file are subject to the Mozilla Public License Version 1.1
+(the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is the TSO CityServer.
+
+The Initial Developer of the Original Code is
+Mats 'Afr0' Vederhus. All Rights Reserved.
+
+Contributor(s): ______________________________________.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Collections;
@@ -13,8 +29,11 @@ namespace TSO_CityServer.Network
         public static SharedArrayList TransferringClients;
         public static Listener NetworkListener;
 
+        //Encryption
         public static ECDiffieHellmanCng ServerPrivateKey = new ECDiffieHellmanCng();
         public static byte[] ServerPublicKey = ServerPrivateKey.PublicKey.ToByteArray();
+
+        public static Session CurrentSession = new Session();
 
         static NetworkFacade()
         {
