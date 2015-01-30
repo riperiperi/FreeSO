683b0d0b44433f7384a51049485fb7a9efb6a018
Mats Vederhus <afr0_at_afr0games.com>
2015-01-30 22:05:51 +0100

    Updated schema
    Characters are now connected to houses in DB, yay!

--- Other/libs/LoginDataModel/LoginDataModel/schema.sql
+++ Other/libs/LoginDataModel/LoginDataModel/schema.sql
@@ -4,10 +4,19 @@ CREATE TABLE `account` (
   `Password` varchar(200) NOT NULL,
   `NumCharacters` int(11) DEFAULT NULL,
   PRIMARY KEY (`AccountID`)
-) ENGINE=InnoDB AUTO_INCREMENT=300 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=778 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `house` (
+  `HouseID` int(11) NOT NULL AUTO_INCREMENT,
+  `X` int(11) NOT NULL,
+  `Y` int(11) NOT NULL,
+  `Flags` tinyint(4) NOT NULL,
+  PRIMARY KEY (`HouseID`),
+  KEY `GUID` (`HouseID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `character` (
-  `CharacterID` int(10) NOT NULL,
+  `CharacterID` int(10) NOT NULL AUTO_INCREMENT,
   `AccountID` int(10) NOT NULL,
   `GUID` varchar(36) NOT NULL DEFAULT '0',
   `LastCached` datetime NOT NULL,
@@ -17,30 +26,19 @@ CREATE TABLE `character` (
   `HeadOutfitID` bigint(20) NOT NULL,
   `BodyOutfitID` bigint(20) NOT NULL,
   `AppearanceType` int(11) NOT NULL,
-  `City` varchar(50) DEFAULT '0',
-  `CityName` varchar(65) DEFAULT NULL,
-  `CityThumb` bigint(20) DEFAULT NULL,
-  `CityMap` bigint(20) DEFAULT NULL,
-  `CityIP` varchar(16) DEFAULT NULL,
-  `CityPort` int(11) DEFAULT NULL,
+  `City` varchar(50) NOT NULL DEFAULT 'c78d14c4-a153-4a44-8b5e-f0c602e50f95',
+  `CityName` varchar(65) NOT NULL DEFAULT 'East Jerome',
+  `CityThumb` bigint(20) NOT NULL DEFAULT '197158417324972',
+  `CityMap` bigint(20) NOT NULL DEFAULT '10531259809793',
+  `CityIP` varchar(16) NOT NULL DEFAULT '173.248.136.133',
+  `CityPort` int(11) NOT NULL DEFAULT '2107',
   `House` int(11) DEFAULT NULL,
   PRIMARY KEY (`CharacterID`),
   UNIQUE KEY `Name` (`Name`,`CharacterID`),
   UNIQUE KEY `GUID` (`GUID`),
-  KEY `FK_character_account` (`AccountID`),
-  KEY `FK_character_house_idx` (`House`),
-  CONSTRAINT `FK_character_house` FOREIGN KEY (`House`) REFERENCES `house` (`GUID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_character_account` FOREIGN KEY (`AccountID`) REFERENCES `account` (`AccountID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `house` (
-  `GUID` int(11) NOT NULL DEFAULT '0',
-  `X` int(11) NOT NULL,
-  `Y` int(11) NOT NULL,
-  `ThumbnailID` int(11) NOT NULL,
-  PRIMARY KEY (`GUID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-USE tsocity;
+  KEY `House_idx` (`House`),
+  CONSTRAINT `House` FOREIGN KEY (`House`) REFERENCES `house` (`HouseID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1499 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `character` (
   `CharacterID` int(10) NOT NULL AUTO_INCREMENT,
