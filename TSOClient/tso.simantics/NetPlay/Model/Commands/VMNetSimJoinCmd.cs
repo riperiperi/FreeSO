84258913c949ff1c1398dec0d9ade844fa2f475b
RHY3756547 <rhy3756547@hotmail.com>
2016-05-17 15:54:28 +0100

    [Netplay] Up version number.

--- TSOClient/tso.simantics/NetPlay/Model/Commands/VMNetSimJoinCmd.cs
+++ TSOClient/tso.simantics/NetPlay/Model/Commands/VMNetSimJoinCmd.cs
@@ -25,7 +25,7 @@ namespace FSO.SimAntics.NetPlay.Model.Commands
         public bool Gender;
         public string Name;
 
-        public static ushort CurVer = 0xFFF4;
+        public static ushort CurVer = 0xFFF3;
 
         public override bool Execute(VM vm)
         {
