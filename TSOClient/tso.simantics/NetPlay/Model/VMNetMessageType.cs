488d621884e05d540ed070bf0edae60fb0b57f6c
RHY3756547 <rhy3756547@hotmail.com>
2016-06-13 15:47:46 +0100

    [VMNet] Add message type

---
+++ TSOClient/tso.simantics/NetPlay/Model/VMNetMessageType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSO.SimAntics.NetPlay.Model
+{
+    public enum VMNetMessageType : byte
+    {
+        //server -> client
+        BroadcastTick = 0,
+        Direct = 1,
+        
+        //client -> server
+        Command = 128
+    }
+}
