d3484f1eb617d459209e13da0aa16b0e2ab94455
TheArchitectFreeSO <ktg221@nyu.edu>
2018-02-23 18:47:22 -0500

    TSOState.Budget for Multitile Objects
    Thanks

--- TSOClient/tso.simantics/NetPlay/Model/Commands/VMNetAsyncResponseCmd.cs
+++ TSOClient/tso.simantics/NetPlay/Model/Commands/VMNetAsyncResponseCmd.cs
@@ -54,9 +54,29 @@ namespace FSO.SimAntics.NetPlay.Model.Commands
                 {
                     var state = (VMTransferFundsState)State;
                     var obj1 = vm.GetObjectByPersist(state.UID1);
-                    if (obj1 != null) obj1.TSOState.Budget.Value = state.Budget1;
+                    if (obj1 != null)
+                    {
+                        if (obj1.MultitileGroup.MultiTile)
+                        {
+                            var objects = new List<VMEntity>(obj1.MultitileGroup.Objects);
+                            foreach (var obj in objects)
+                                obj.TSOState.Budget.Value = state.Budget1;
+                        }
+                        else
+                            obj1.TSOState.Budget.Value = state.Budget1;
+                    }
                     var obj2 = vm.GetObjectByPersist(state.UID2);
-                    if (obj2 != null) obj2.TSOState.Budget.Value = state.Budget2;
+                    if (obj2 != null)
+                    {
+                        if (obj2.MultitileGroup.MultiTile)
+                        {
+                            var objects = new List<VMEntity>(obj2.MultitileGroup.Objects);
+                            foreach (var obj in objects)
+                                obj.TSOState.Budget.Value = state.Budget2;
+                        }
+                        else
+                            obj2.TSOState.Budget.Value = state.Budget2;
+                    }
                 }
             }
 
