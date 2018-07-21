d24cb924392ff41813babc5f5c391ebb59011ad3
ItsSim <dbhdane@outlook.com>
2018-07-21 12:13:53 +0200

    After client_id

--- TSOClient/FSO.Server.Database/DatabaseScripts/changes/0022_lastlogin.sql
+++ TSOClient/FSO.Server.Database/DatabaseScripts/changes/0022_lastlogin.sql
@@ -1,2 +1,2 @@
 ﻿ALTER TABLE `fso_users` 
-ADD COLUMN `last_login` INT(10) UNSIGNED NOT NULL DEFAULT 0 AFTER `last_ip`;
+ADD COLUMN `last_login` INT(10) UNSIGNED NOT NULL DEFAULT 0 AFTER `client_id`;
