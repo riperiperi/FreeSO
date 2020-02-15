762f59f2ae8e27de7910d4ea30b0226c63550cce
TheArchitectFreeSO <ktg221@nyu.edu>
2020-02-15 09:35:34 -0500

    GameshowBuzzerEOD: Initial Commit
    Empty client and server files commit to allow working at both stations.

---
+++ TSOClient/tso.client/UI/Panels/EODs/UIGameshowBuzzerEOD.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSO.Client.UI.Panels.EODs
+{
+    public class UIGameshowBuzzerEOD : UIEOD
+    {
+        // Host assets
+
+
+        public UIGameshowBuzzerEOD(UIEODController controller) : base(controller)
+        {
+            InitUI();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void InitUI()
+        {
+
+        }
+    }
+}
---
+++ TSOClient/tso.simantics/NetPlay/EODs/Handlers/VMEODGameshowBuzzerPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSO.SimAntics.NetPlay.EODs.Handlers
+{
+    public class VMEODGameshowBuzzerPlugin : VMEODHandler
+    {
+        public VMEODGameshowBuzzerPlugin(VMEODServer server) : base(server)
+        {
+            
+        }
+    }
+}
