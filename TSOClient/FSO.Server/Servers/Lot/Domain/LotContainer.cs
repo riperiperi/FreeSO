5367d12e8f61e782b4c8dc746555c19d224c37e1
riperiperi <rhy3756547@hotmail.com>
2022-11-07 19:04:51 +0000

    Add hide and search controller to controller IDs

--- TSOClient/FSO.Server/Servers/Lot/Domain/LotContainer.cs
+++ TSOClient/FSO.Server/Servers/Lot/Domain/LotContainer.cs
@@ -113,6 +113,7 @@ namespace FSO.Server.Servers.Lot.Domain
             0x04AB1D1F, //zombie spawner controller
             0x0DF8592E, //mini sims
             0x48F32C7B, //au game controller
+            0x3307FE5B, //hide and search controller
 
             0x5157DDF2, //cat carrier
             0x3278BD34, //dog carrier
